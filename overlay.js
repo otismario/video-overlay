4e286ee7eaaee9bc0a4a04dc6052086bdf46b280
Otis Mario <lilmariobros@gmail.com>
2020-02-26 14:53:25 +0400

    Completed the project of creating a video overlay

---
+++ overlay.js
@@ -0,0 +1,7 @@
+//This calls a greeting function to display on the click of the create profile button 
+document.getElementById("submit_btn").addEventListener("click", greeting);
+
+function greeting(){
+        alert("Thank you for Completing the video overlay tutorial");
+        console.log("Thank you for Completing the video overlay tutorial");
+}
